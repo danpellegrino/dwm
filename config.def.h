8bae5b859daa5559fc232457e0215f993bbd89b3
Daniel <danpellegrino1@gmail.com>
2023-05-26 05:31:39 +0000

    updated tab icons

--- config.def.h
+++ config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
-#define FONT "Hack Nerd Font Mono"
+#define FONT "JetBrainsMono Nerd Font"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { FONT":size=10" };
+static const char *fonts[]          = { FONT":size=12" };
 static const char dmenufont[]       = FONT":size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "󰈹", "󰓓", "󰲌", "" };
+static const char *tags[] = { "", "󰈹", "󰓓", "󰲌", "", "󰙯" };
 
 static const Rule rules[] = {
 	/* xprop(1):
